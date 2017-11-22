cb93b30b3916e4f4588943927f01771f2f11cbbb
Angelika Maria Piątkowska (piatkosia) <piatkosia.apt@interia.pl>
2017-11-22 14:44:59 +0100

    Tak się olewa protection level

---
+++ SecureClass/VerySecureClass.cs
@@ -0,0 +1,27 @@
+﻿
+namespace FindMeIfYouCan
+{
+    class VerySecureClass
+    {
+        public override string ToString()
+        {
+            return " To jest niezbyt cwane";
+        }
+        private string GetHiddenString()
+        {
+            return " Ooo to rozumiem";
+        }
+
+        private class HyperSecureClass
+        {
+            public override string ToString()
+            {
+                return " A kuku";
+            }
+            private string GetMoreHiddenString()
+            {
+                return " I co, tego nie widać?";
+            }
+        }
+    }
+}
--- olejTo/Program.cs
+++ olejTo/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace olejTo
 {
@@ -10,6 +7,20 @@ namespace olejTo
     {
         static void Main(string[] args)
         {
+            var Asm = Assembly.Load("SecureClass");
+            var types = Asm.GetTypes();
+            var SingleType = types[0];
+            var FunnyObject = Convert.ChangeType(Activator.CreateInstance(SingleType), SingleType);
+            Console.WriteLine(FunnyObject);
+            var MethodInfoF = SingleType.GetMethod("GetHiddenString", BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine(MethodInfoF.Invoke(FunnyObject, null));
+            //to teraz zejdziemy sobie odpowiednio głębiej
+            Type nested = SingleType.GetNestedType("HyperSecureClass", BindingFlags.NonPublic | BindingFlags.Instance);
+            var StrangeObject = Convert.ChangeType(Activator.CreateInstance(nested), nested);
+            var MethodInfoS = nested.GetMethod("GetMoreHiddenString", BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine(StrangeObject);
+            Console.WriteLine(MethodInfoS.Invoke(StrangeObject, null));
+            Console.ReadKey();
         }
     }
 }
